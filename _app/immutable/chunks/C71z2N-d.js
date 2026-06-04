42948b04ef5f5dbf61e52d600b1521a67cc90de9
Roberto Vallado <20017482@saultcollege.ca>
2026-06-04 13:00:30 -0400

    Updates

---
+++ _app/immutable/chunks/C71z2N-d.js
@@ -0,0 +1,1 @@
+import{s as e}from"./1M62nm3w.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
---
+++ _app/immutable/entry/start.BYg_dAcG.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/1M62nm3w.js";export{o as load_css,r as start};
